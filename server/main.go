784beb911416621a98648bba296cd31a3ab33ea3
Mitchell O'Hair <mitchell.ohair@gmail.com>
2020-01-04 16:04:39 -0800

    Formatted time output

--- server/main.go
+++ server/main.go
@@ -5,21 +5,12 @@ import (
 	"time"
 	"context"
 	"net/http"
-	"path"
 	"strings"
+	"math"
 
 	"github.com/google/go-github/github"
 	"golang.org/x/oauth2"
 )
-
-func getPath(p string) (head, tail string) {
-	p = path.Clean("/" + p)
-	i := strings.Index(p[1:], "/") + 1
-	if i <= 0 {
-		return p[1:], "/"
-	}
-	return p[1:i], p[i:]
-}
 
 func main() {
 	http.HandleFunc("/latest", func(w http.ResponseWriter, r *http.Request) {
@@ -27,19 +18,30 @@ func main() {
     if (r.Method == "OPTIONS") {
         w.Header().Set("Access-Control-Allow-Headers", "Authorization")
     } else {
-			user := strings.Join(r.URL.Query()["username"], "")
-			ctx := context.Background()
+			user, ctx := strings.Join(r.URL.Query()["username"], ""), context.Background()
 			tc := oauth2.NewClient(ctx, nil)
 			client := github.NewClient(tc)
+
 			repos, _, err := client.Repositories.List(ctx, user, &github.RepositoryListOptions{Type:"owner", Sort:"updated"})
 			if err != nil {
 				fmt.Printf("couldn't gather clients repositories failed with %s", err)
 				return
 			}
-			res := time.Duration.String(time.Now().Local().Sub(repos[0].GetUpdatedAt().Local()))
-			fmt.Println(res)
-			w.Write([]byte(res))
+
+			diff := time.Duration.Hours(time.Now().Local().Sub(repos[0].GetUpdatedAt().Local()))
+			if diff < 1 {
+				w.Write([]byte("Less than an hour ago"))
+			} else {
+				suff := "hours"
+				if diff > 24 {
+					diff /= 24
+					suff = "days"
+				}
+				res := fmt.Sprint(math.Round(diff))
+				w.Write([]byte(res + " " + suff + " ago"))
+			}
     }
 	})
+
 	fmt.Println(http.ListenAndServe(":8080", nil))
 }
