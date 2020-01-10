2b1b3704efdbfb88a60e7f91cf18273a6ae3be82
Mitchell O'Hair <mitchell.ohair@gmail.com>
2020-01-09 22:47:21 -0800

    Deploy

--- server/main.go
+++ server/main.go
@@ -14,33 +14,28 @@ import (
 
 func main() {
 	http.HandleFunc("/latest", func(w http.ResponseWriter, r *http.Request) {
-		 w.Header().Set("Access-Control-Allow-Origin", "*")
-    if (r.Method == "OPTIONS") {
-        w.Header().Set("Access-Control-Allow-Headers", "Authorization")
-    } else {
-			user, ctx := strings.Join(r.URL.Query()["username"], ""), context.Background()
-			tc := oauth2.NewClient(ctx, nil)
-			client := github.NewClient(tc)
+		user, ctx := strings.Join(r.URL.Query()["username"], ""), context.Background()
+		tc := oauth2.NewClient(ctx, nil)
+		client := github.NewClient(tc)
 
-			repos, _, err := client.Repositories.List(ctx, user, &github.RepositoryListOptions{Type:"owner", Sort:"updated"})
-			if err != nil {
-				fmt.Printf("couldn't gather clients repositories failed with %s", err)
-				return
-			}
+		repos, _, err := client.Repositories.List(ctx, user, &github.RepositoryListOptions{Type:"owner", Sort:"updated"})
+		if err != nil {
+			fmt.Printf("couldn't gather clients repositories failed with %s", err)
+			return
+		}
 
-			diff := time.Duration.Hours(time.Now().Local().Sub(repos[0].GetUpdatedAt().Local()))
-			if diff < 1 {
-				w.Write([]byte("Less than an hour ago"))
-			} else {
-				suff := "hours"
-				if diff > 24 {
-					diff /= 24
-					suff = "days"
-				}
-				res := fmt.Sprint(math.Round(diff))
-				w.Write([]byte(res + " " + suff + " ago"))
+		diff := time.Duration.Hours(time.Now().Local().Sub(repos[0].GetUpdatedAt().Local()))
+		if diff < 1 {
+			w.Write([]byte("Less than an hour ago"))
+		} else {
+			suff := "hours"
+			if diff > 24 {
+				diff /= 24
+				suff = "days"
 			}
-    }
+			res := fmt.Sprint(math.Round(diff))
+			w.Write([]byte(res + " " + suff + " ago"))
+		}
 	})
 
 	fmt.Println(http.ListenAndServe(":8080", nil))
